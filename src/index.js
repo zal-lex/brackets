34eb36712d85bdc193da0867e25206541a811ff5
Alexey Zalyotov <alexey.zalyotov@rubyroidlabs.com>
2021-03-16 02:24:07 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let arrayFromString = str.split('');
+  let result = true;
+  for (let i = 0; i < arrayFromString.length; i++) {
+    let currentBracket = arrayFromString[i];
+    bracketsConfig.forEach((configSet) => {
+      if (configSet.includes(currentBracket)) {
+        if (configSet[0] === configSet[1]) {
+          stack[stack.length - 1] === currentBracket ? stack.pop() : stack.push(currentBracket);
+        } else if (configSet[0] === currentBracket) {
+          stack.push(currentBracket);
+        } else {
+          if (stack[stack.length - 1] === configSet[0]) {
+            stack.pop();
+          } else {
+            result = false;
+          }
+        }
+      }
+    })
+  }
+  if (stack.length > 0) { result = false }
+  return result;
 }
